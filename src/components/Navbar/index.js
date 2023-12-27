ae8a125e1d81e905279b47dbdbb40ef8664364e5
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-27 19:13:21 +0530

    molile device components added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,11 @@
 'use client';
 import { adminNavOptions } from '@/utils';
 import { func } from 'joi';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { navOptions } from '@/utils';
 import { styles } from '@/utils';
+import { GlobalContext } from '@/context';
+import CommonModal from '../CommonModal';
 
 const isAdminView = false;
 const isAuthUser = true;
@@ -12,10 +14,17 @@ const user = {
 }
 
 
-function NavItems() {
+function NavItems({ isModalView = false }) {
     return (
-        <div className='items-center justify-between w-full md:flex md:w-auto' id='nav-items'>
-            <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white'>
+        <div
+            className={`items-center justify-between w-full md:flex md:w-auto ${isModalView ? "" : "hidden"
+                }`}
+            id="nav-items"
+        >
+            <ul
+                className={`flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${isModalView ? "border-none" : "border border-gray-100"
+                    }`}
+            >
                 {
                     isAdminView ?
                         (
@@ -35,7 +44,7 @@ function NavItems() {
                         )
                 }
             </ul>
-        </div>
+        </div >
     )
 }
 
@@ -43,61 +52,77 @@ function NavItems() {
 
 
 export default function Navbar() {
+    const { showNavModal, setShowNavModal } = useContext(GlobalContext)
     return (
-        <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <div className='flex items-center cursor-pointer'>
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap">
-                        QUINOVA
-                    </span>
-                </div>
-                <div className="flex md:order-2 gap-2">
-                    {
-                        (isAuthUser && !isAdminView) ?
-                            (
-                                <Fragment>
-                                    <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Account</button>
-                                    <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Cart</button>
-                                </Fragment>
-                            )
-                            : null
-                    }
-                    {
-                        (user.role === "admin") ?
-                            (isAdminView ? <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Client View</button> : <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Admin View</button>)
-                            : null
-                    }
-                    {
-                        (isAuthUser ? (<button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Logout</button>) : (<button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Login</button>))
-                    }
+        <>
+            <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div className='flex items-center cursor-pointer'>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                            QUINOVA
+                        </span>
+                    </div>
+                    <div className="flex md:order-2 gap-2">
+                        {
+                            (isAuthUser && !isAdminView) ?
+                                (
+                                    <Fragment>
+                                        <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Account</button>
+                                        <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Cart</button>
+                                    </Fragment>
+                                )
+                                : null
+                        }
+                        {
+                            (user.role === "admin") ?
+                                (isAdminView ? <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Client View</button> : <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Admin View</button>)
+                                : null
+                        }
+                        {
+                            (isAuthUser ? (<button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Logout</button>) : (<button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide rounded-lg text-white"}>Login</button>))
+                        }
 
 
-                    <button
-                        data-collapse-toggle="navbar-sticky"
-                        type="button"
-                        className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-sticky"
-                        aria-expanded="false"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-6 h-6"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
+                        <button
+                            data-collapse-toggle="navbar-sticky"
+                            type="button"
+                            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="navbar-sticky"
+                            aria-expanded="false"
+                            onClick={() => setShowNavModal(true)}
                         >
-                            <path
-                                fill-rule="evenodd"
-                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
-                    </button>
-                </div>
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className="w-6 h-6"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </button>
+                    </div>
 
-                <NavItems></NavItems>
-            </div>
-        </nav>
+                    <NavItems isModal={false}></NavItems>
+                </div>
+            </nav>
+            <CommonModal
+                showModalTitle={false}
+                mainContent={
+                    <NavItems
+                        isModalView={true}
+                    // isAdminView={isAdminView}
+                    />
+                }
+                show={showNavModal}
+                setShow={setShowNavModal}
+            />
+        </>
+
     )
 }
--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,12 @@
 'use client';
 import { createContext } from "react";
-
+import { useState } from "react";
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
+    const [showNavModal, setShowNavModal] = useState(false);
     return (
-        <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>
+        <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>{children}</GlobalContext.Provider>
     )
 }
